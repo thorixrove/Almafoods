581b04246cba41f25e9dbbbd5516ee7fb7d358f9
Thorix Rover <thorixrover@gmail.com>
2026-07-15 07:53:32 +0900

    refactor: remove unused components and files, streamline app structure
    - Deleted _layout.tsx, explore.tsx, index.tsx, and associated components to simplify the app.
    - Removed animated-icon and related styles, consolidating splash screen functionality.
    - Eliminated app-tabs and web-badge components for a cleaner navigation experience.
    - Cleared out external-link, hint-row, themed-text, themed-view, and collapsible components.
    - Removed theme constants and global CSS, transitioning to Tailwind CSS for styling.
    - Updated tsconfig to include nativewind-env.d.ts for Tailwind integration.

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View} from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to Almafoods
+      </Text>
+    </View>
+
+  );
+}
+
+
